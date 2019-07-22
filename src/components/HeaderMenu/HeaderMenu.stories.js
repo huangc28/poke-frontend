c9317c0a7b464573b9f007395fe5efcf6c48a6a2
Huang Chi-Han <huangchiheng@gmail.com>
2019-07-22 22:02:09 +0800

    Add story book decorator

--- src/components/HeaderMenu/HeaderMenu.stories.js
+++ src/components/HeaderMenu/HeaderMenu.stories.js
@@ -3,7 +3,16 @@ import { storiesOf } from '@storybook/react'
 
 import HeaderMenu from './HeaderMenu'
 
+const styles = {
+  height: 200,
+  display: 'flex',
+  'justify-content': 'center',
+  'align-items': 'center',
+}
+const HeaderMenuDecorator = storyFn => <div style={styles}>{storyFn()}></div>
+
 storiesOf('Header Menu', module)
+  .addDecorator(HeaderMenuDecorator)
   .add('Header Menu', () => (
     <HeaderMenu />
   ))
