fa882e0e022b192548e06c99bf2484072ef4a216
willy11342002 <willy11342002@gmail.com>
2019-09-27 15:18:18 +0800

    remove require from sage, redux

--- src/services/redux/index.js
+++ src/services/redux/index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux'
 
 import hotArticles from './hotArticles'
-// import knowledge from '../../scenes/Knowledge/services/redux'
-import foodKnowledge from '../../scenes/FoodKnowledge/services/redux'
 import singleArticle from '../../scenes/SingleArticle/services/redux/singleArticle'
 
 export default combineReducers({
-//   knowledge,
   hotArticles,
-  foodKnowledge,
   singleArticle,
 })
--- src/services/sagas/index.js
+++ src/services/sagas/index.js
@@ -1,18 +1,11 @@
 import { all, call } from 'redux-saga/effects'
 
-// import knowledge from '../../scenes/Knowledge/services/sagas'
-
-import foodKnowledge from '../../scenes/FoodKnowledge/services/sagas'
-
 import hotArticles from './hotArticles'
-
 import singleArticle from '../../scenes/SingleArticle/services/sagas'
 
 export default function * () {
   yield all([
-    // call(knowledge),
     call(hotArticles),
-    call(foodKnowledge),
     call(singleArticle),
   ])
 }
