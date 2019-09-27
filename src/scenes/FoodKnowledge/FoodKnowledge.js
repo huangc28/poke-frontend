5608b89db3fba04970ca97c58009ed85a4c49caf
willy11342002 <willy11342002@gmail.com>
2019-09-27 15:19:04 +0800

    remove redux-saga frame from FoodKnowledge

--- src/scenes/FoodKnowledge/FoodKnowledge.js
+++ src/scenes/FoodKnowledge/FoodKnowledge.js
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import T from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from 'react'
 import styled from 'styled-components'
-import { compose } from 'redux'
-import { withRouter } from 'react-router'
+import $ from 'jquery'
 
-import Main from '@poke/layouts/Main'
+import Main from '../../layouts/Main/Main'
+import Img from '@poke/components/Img'
 import TopArticleLayout from '@poke/layouts/TopArticleLayout'
-import ContactLayout from '@poke/layouts/ContactLayout'
-import { size26Mixin, size14Mixin } from '@poke/styles/font'
-import colors from '@poke/styles/colors'
 import HotArticles from '@poke/components/HotArticles'
+import ArticleGrid from '@poke/components/ArticleGrid'
+import ContactLayout from '@poke/layouts/ContactLayout'
+import { buildApiUrl } from '@poke/services/apis/util'
 import IconLabel from '@poke/components/IconLabel'
+import convertDateTimeStringToTimestamp from '@poke/util/convertDateTimeStringToTimestamp'
+import TimeAgo from '@poke/components/TimeAgo'
+import { size14Mixin, size26Mixin } from '@poke/styles/font'
+import colors from '@poke/styles/colors'
 import { DateWhite, PPWhite, More as MoreIcon } from '@poke/components/Icons'
 import Paginator from '@poke/components/Paginator'
-import ArticleGrid from '@poke/components/ArticleGrid'
-import TimeAgo from '@poke/components/TimeAgo'
-import Img from '@poke/components/Img'
-import { PER_PAGE } from '@poke/services/constants/articles'
-import { insetShadow } from '@poke/styles/shadow'
-import convertDateTimeStringToTimestamp from '@poke/util/convertDateTimeStringToTimestamp'
 
-import {
-  fetchCanArticles,
-  selectCanArticles,
-  selectCanArticlesTotalCount
-} from './services/redux/canArticles'
-import {
-  fetchDryFoodArticles,
-  selectDryFoodArticles,
-  selectDryFoodArticlesTotalCount
-} from './services/redux/dryFoodArticles'
-
-import Tabs, { TabBar, Tab, TabPanel } from './components/Tabs'
 
 const SectionOne = styled.div`
   min-height: 33rem;
 `
+
 const SectionTwo = styled.div`
   min-height: 30rem;
+  height: 600px;
+  background-color: #3c4e6b;
+`
+
+const Topic = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 0px;
+`
+
+const Tab = styled.div`
+  width: 154px;
+  border-radius: 20px 20px 0px 0px;
+  text-align: center;
+  font-size: 20px;
+  line-height: 0.5rem;
+  cursor: pointer;
 `
 
 const IntroContainer = styled.div`
@@ -52,6 +56,25 @@ const Title = styled.p`
   letter-spacing: 0.8125rem;
 `
 
+
+const ArticlesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 7.4375rem 3.875rem 0;
+  & > div {
+    margin-bottom: 4.625rem;
+  }
+`
+
+const PaginatorContainer = styled.div`
+  padding-bottom: 3.125rem;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+`
+
 const ArticleStat = styled.div`
   display: flex;
   margin-top: 1.5rem;
@@ -61,7 +84,7 @@ const SummaryContent = styled.div`
   flex-direction: column;
 `
 
-const Summary = styled.div`
+const Summary = styled.span`
   ${size14Mixin}
   color: ${colors.white};
   margin: 1.625rem 0 0 0;
@@ -76,288 +99,179 @@ const More = styled.div`
   margin: 2.25rem 0 0 0;
 `
 
-const ArticlesContainer = styled.div`
-  ${insetShadow}
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 7.4375rem 3.875rem 0;
-  & > div {
-    margin-bottom: 4.625rem;
-  }
-`
-const PaginatorContainer = styled.div`
-  padding-bottom: 3.125rem;
+const PrevArrow = styled.div`
+  
 `
 
-const DRY_FOOD_TAB_ID = 'dry_food_tab'
-const CAN_FOOD_TAB_ID = 'can_food_tab'
+class FoodKnowledge extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            category: '飯飯學問大',
+            PER_PAGE: 5,
+            total_page: 1,
+            articles: [0,1,2,3,4].map((v,i)=>{return {title:'沒有新文章'}})
+        }
+        this.ajaxArticle = this.ajaxArticle.bind(this)
+        this.countTotalPages = this.countTotalPages.bind(this)
+    }
 
-function countTotalPages (perPage, totalCount) {
-  const residual = totalCount % perPage
-  let pages = Math.floor(totalCount / perPage)
+    countTotalPages (perPage, totalCount) {
+        const residual = totalCount % perPage
+        let pages = Math.floor(totalCount / perPage)
+      
+        if (residual > 0) {
+          pages++
+        }
+      
+        return pages
+    }
 
-  if (residual > 0) {
-    pages++
-  }
+    ajaxArticle({ offset, category='飯飯學問大' }){
+        this.setState({ category })
+        let params = {
+            category: category,
+            sort: 'created_at',
+            limit: this.state.PER_PAGE,
+            offset,
+        }
+        let apiUrl = buildApiUrl('articles', params)
+        
+        $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            success: function(data){ 
+                let pageCount = this.countTotalPages(this.state.PER_PAGE, data.total_count)
+                this.setState({
+                    articles: this.state.articles.map((v,i) => {return data.articles[i] ? data.articles[i] : {'title': '沒有更多文章了'}}),
+                    total_page: pageCount,
+                })
+            }.bind(this),
+            error: function(err){ 
+                throw err
+            }.bind(this)
+        })
+    }
 
-  return pages
-}
+    render() {
+        let first = this.state.articles[0]
+        let firstArticle = (
+            <TopArticleLayout
+                left={
+                    <React.Fragment>
+                        <Topic>
+                            <Tab style={{ backgroundColor: this.state.category=='乾飼料資料庫' ? colors.pink : colors.gray }}
+                                 onClick={() => {this.ajaxArticle({ offset:0, category:'乾飼料資料庫' })}}>
+                                <p>乾飼料資料庫</p>
+                            </Tab>
+                            <Tab style={{ backgroundColor: this.state.category=='罐罐資料庫' ? colors.pink : colors.gray }}
+                                 onClick={() => {this.ajaxArticle({ offset:0, category:'罐罐資料庫' })}}>
+                                 <p>罐罐資料庫</p>
+                            </Tab>
+                        </Topic>
 
-function FoodKnwledge ({
-  fetchCanArticles,
-  fetchDryFoodArticles,
-  canArticles,
-  canArticlesTotalCount,
-  dryFoodArticles,
-  dryFoodArticlesTotalCount,
-  history,
-}) {
-  const [topArticle, setTopArticle] = useState({})
-  const [restArticles, setRestArticles] = useState([])
-  const [articleTotalCount, setArticleTotalCount] = useState(dryFoodArticlesTotalCount)
-  const [articleFetcher, setArticleFetcher] = useState(fetchDryFoodArticles)
-  const [tabID, setActiveTab] = useState(DRY_FOOD_TAB_ID)
-
-  const getDisplayArticles = tabID => tabID === DRY_FOOD_TAB_ID
-    ? dryFoodArticles
-    : canArticles
-
-  const getArticleFetcher = tabID => tabID === DRY_FOOD_TAB_ID
-    ? fetchDryFoodArticles
-    : fetchCanArticles
-
-  const getTabbedArticleTotalCount = tabID => tabID === DRY_FOOD_TAB_ID
-    ? canArticlesTotalCount
-    : dryFoodArticlesTotalCount
-
-  useEffect(() => {
-    fetchCanArticles()
-    fetchDryFoodArticles()
-  }, [])
-
-  useEffect(() => {
-    const displayArticles = getDisplayArticles(tabID)
-    const top = displayArticles[0] || {}
-    const rest = displayArticles.slice(1)
-    setTopArticle(top)
-    setRestArticles(rest)
-
-    const articleFetcher = getArticleFetcher(tabID)
-    setArticleFetcher(articleFetcher)
-
-    const articleTotalCount = getTabbedArticleTotalCount(tabID)
-    setArticleTotalCount(articleTotalCount)
-  }, [canArticles.length, dryFoodArticles.length, tabID])
-
-  return (
-    <Main>
-      <SectionOne>
-        <HotArticles
-          onClickArticle={({ articleID }) => history.push(`/articles/${articleID}`)}
-        />
-      </SectionOne>
-
-      <SectionTwo>
-        <TopArticleLayout
-          left={
-            <React.Fragment>
-              <Tabs defaultTab={DRY_FOOD_TAB_ID}>
-                <TabBar>
-                  <Tab
-                    id={DRY_FOOD_TAB_ID}
-                    onClick={evt => {
-                      setActiveTab(DRY_FOOD_TAB_ID)
-                      fetchCanArticles()
-                    }}
-                  >
-                    乾飼料資料庫
-                  </Tab>
-                  <Tab
-                    id={CAN_FOOD_TAB_ID}
-                    onClick={evt => {
-                      setActiveTab(CAN_FOOD_TAB_ID)
-                      fetchDryFoodArticles()
-                    }}
-                  >
-                    罐罐料資料庫
-                  </Tab>
-                </TabBar>
-
-                <TabPanel
-                  whenActive={DRY_FOOD_TAB_ID}
-                >
-                  <IntroContainer
-                    onClick={
-                      () => history.push(`/articles/${topArticle.article_id}`)
-                    }
-                  >
-                    <Img
-                      src={topArticle.img}
-                      width={600}
-                      height={420}
-                      fallbackImgWidth={600}
-                      fallbackImgHeight={420}
-                    />
-                  </IntroContainer>
-                  <div style={{ clear: 'both' }} />
-                </TabPanel>
-
-                <TabPanel
-                  whenActive={CAN_FOOD_TAB_ID}
-                >
-                  <IntroContainer
-                    onClick={
-                      () => history.push(`/articles/${topArticle.article_id}`)
-                    }
-                  >
-                    <Img
-                      src={topArticle.img}
-                      width={600}
-                      height={420}
-                      fallbackImgWidth={600}
-                      fallbackImgHeight={420}
-                    />
-                  </IntroContainer>
-                  <div style={{ clear: 'both' }} />
-                </TabPanel>
-              </Tabs>
-            </React.Fragment>
-          }
-          right={
-            <React.Fragment>
-              <Title>
-                {topArticle.title}
-              </Title>
-
-              <ArticleStat>
-                <IconLabel
-                  icon={
-                    <DateWhite
-                      width={16}
-                      height={16}
-                    />
-                  }
-                  label={
-                    <TimeAgo
-                      time={
-                        topArticle.updated_at
-                          ? convertDateTimeStringToTimestamp(topArticle)
-                          : Date.now()
-                      }
-                    />
-                  }
-                />
-
-                <IconLabel
-                  icon={
-                    <PPWhite
-                      width={16}
-                      height={16}
-                    />
-                  }
-                  label={topArticle.visit}
-                />
-              </ArticleStat>
-
-              <SummaryContent>
-                <Summary>
-                  <div dangerouslySetInnerHTML={{ __html: topArticle.descript }} />
-                </Summary>
-
-                <More>
-                  <IconLabel
-                    icon={
-                      <MoreIcon
-                        width={16}
-                        height={16}
-                      />
-                    }
-                    label='more'
-                    onClick={() => history.push(`/articles/${topArticle.article_id}`)}
-                  />
-                </More>
-              </SummaryContent>
-            </React.Fragment>
-          }
-        />
-      </SectionTwo>
-
-      <ContactLayout>
-        <ArticlesContainer>
-          {
-            restArticles.map((article, index) => (
-              <ArticleGrid
-                key={index}
-                tagNum={index + 1}
-                title={article.title}
-                summary={article.descript}
-                timeAgo={article.updated_at || ''}
-                numViewed={article.visit}
-                img={
-                  <Img
-                    src={article.img}
-                    fallbackImgWidth={300}
-                    fallbackImgHeight={200}
-                  />
+                        <IntroContainer onClick={() => location.href = `/articles/${first.article_id}`} >
+                            <Img
+                            src={ first.img || `https://via.placeholder.com/${600}x${420}`}
+                            width={600}
+                            height={420}
+                            fallbackImgWidth={600}
+                            fallbackImgHeight={420}
+                            style={{ cursor: 'pointer' }}
+                            />
+                        </IntroContainer>
+                        <div style={{ clear: 'both' }} />
+                    </React.Fragment>
                 }
-                onClickMore={() => history.push(`/articles/${article.article_id}`)}
-              />
-            ))
-          }
-        </ArticlesContainer>
-        <PaginatorContainer>
-          <Paginator
-            breakLabel={'...'}
-            pageCount={countTotalPages(PER_PAGE, articleTotalCount)}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={({ selected }) => {
-              articleFetcher({
-                offset: selected * PER_PAGE
-              })
-            }}
-          />
-        </PaginatorContainer>
-      </ContactLayout>
-    </Main>
-  )
+
+                right={
+                    <React.Fragment>
+                        <Title onClick={() => location.href = `/articles/${first.article_id}`}> 
+                            { first.title } 
+                        </Title>
+                        <ArticleStat>
+                            <IconLabel
+                            icon={ <DateWhite width={16} height={16} /> }
+                            label={<TimeAgo time={
+                                !!first.updated_at
+                                ? convertDateTimeStringToTimestamp(first.updated_at) 
+                                : Date.now()
+                            } />}
+                            />
+                            <IconLabel
+                            icon={ <PPWhite width={16} height={16} /> }
+                            label={ first.visit || 0}
+                            />
+                        </ArticleStat>
+                        
+                        <SummaryContent>
+                            <Summary>
+                            <div dangerouslySetInnerHTML={{ __html: first.descript }} />
+                            </Summary>
+                        
+                            <More>
+                            <IconLabel
+                                icon={ <MoreIcon width={16} height={16} /> }
+                                label='more'
+                                onClick={() => location.href = `/articles/${first.article_id}`}
+                            />
+                            </More>
+                        </SummaryContent>
+                    </React.Fragment>
+                }
+            >
+            </TopArticleLayout>
+        )
+
+        let restArticle = [1,2,3,4].map( (v, i) => {
+            let article = this.state.articles[v] 
+            return (
+                <ArticleGrid
+                    key={i}
+                    tagNum={v+1}
+                    title={article.title}
+                    summary={article.descript}
+                    timeAgo={article.updated_at}
+                    numViewed={article.visit}
+                    onClickMore={() => location.href = `/articles/${article.article_id}` }
+                />
+            )
+        })
+
+        return (
+            <Main>
+                <SectionOne>
+                    <HotArticles
+                      onClickArticle={({ articleID }) => history.push(`/articles/${articleID}`)}
+                    />
+                </SectionOne>
+
+                <SectionTwo>
+                    {firstArticle}
+                </SectionTwo>
+
+                <ContactLayout>
+                    <ArticlesContainer>
+                        {restArticle}
+                    </ArticlesContainer>
+                    <PaginatorContainer>
+                    <Paginator
+                      breakLabel={'...'}
+                      pageCount={this.state.total_page}
+                      marginPagesDisplayed={2}
+                      pageRangeDisplayed={5}
+                      initialPage={0}
+                      onPageChange={({ selected }) => {
+                        this.ajaxArticle({
+                          offset: selected * this.state.PER_PAGE
+                        })
+                      }}
+                    />
+                    </PaginatorContainer>
+                </ContactLayout>
+            </Main>
+        )
+    }
 }
 
-FoodKnwledge.propTypes = {
-  canArticles: T.array,
-  canArticlesTotalCount: T.number,
-
-  dryFoodArticles: T.array,
-  dryFoodArticlesTotalCount: T.number,
-
-  fetchCanArticles: T.func.isRequired,
-  fetchDryFoodArticles: T.func.isRequired,
-
-  history: T.objectOf(T.any).isRequired,
-}
-
-FoodKnwledge.defaultProps = {
-  canArticles: [],
-  canArticlesTotalCount: 0,
-
-  dryFoodArticles: [],
-  dryFoodArticlesTotalCount: 0,
-}
-
-const mapToProps = state => ({
-  canArticles: selectCanArticles(state),
-  canArticlesTotalCount: selectCanArticlesTotalCount(state),
-
-  dryFoodArticles: selectDryFoodArticles(state),
-  dryFoodArticlesTotalCount: selectDryFoodArticlesTotalCount(state)
-})
-
-export default compose(
-  connect(mapToProps, {
-    fetchCanArticles,
-    fetchDryFoodArticles,
-  }),
-  withRouter
-)(FoodKnwledge)
+export default FoodKnowledge
--- src/scenes/FoodKnowledge/services/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import canArticles from './canArticles'
-import dryFoodArticles from './dryFoodArticles'
-
-export default combineReducers({
-  canArticles,
-  dryFoodArticles,
-})
