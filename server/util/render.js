75df24abccb5c5db221baaf8b17b6bb87e030315
Huang Chi-Han <huangchiheng@gmail.com>
2019-11-10 15:38:38 +0800

    Added a meta for google identity service.

--- server/util/render.js
+++ server/util/render.js
@@ -17,6 +17,7 @@ const render = ({
       <head>
         <meta charset="utf-8">
         <title>Poke</title>
+        <meta name="google-site-verification" content="cGOhAHkmx92xY5HewhvTZVaufBeakKs2QbyyLrBsoXw" />
         <meta name="author" content="">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
